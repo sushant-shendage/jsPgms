7e834c6a6f62d897802a7e0500dae8e2d626bcf8
Sushant Shendage <sushantshendage44@gmail.com>
2024-10-06 01:56:12 +0530

    program added

---
+++ numberPgms/PrimeNum.js
@@ -0,0 +1,14 @@
+let num = 7;
+
+for (var i = 2; i < num; i++) {
+    if (num % i == 0) {
+        break;
+    }
+}
+
+if (i == num) {
+    console.log(`${num} is prime number`);
+} else {
+    console.log(`${num} is prime number`);
+
+}
