b857fae07e632ec6a0a8bbe0fce7bc0611543e2a
Sushant Shendage <sushantshendage44@gmail.com>
2024-10-07 13:14:05 +0530

    update

---
+++ jsBasicsQs/datefx/date1fx.js
@@ -0,0 +1,62 @@
+console.log(Date.now());
+for(i=0;i<10;i++)
+    {
+console.log(Date.now());
+    }
+
+
+
+console.log("-console.log(new Date());-");
+console.log(new Date());
+console.log("----------------------------------");
+console.log("-console.log(new Date().getDay());-");  
+console.log(new Date().getDay());
+console.log("----------------------------------");
+console.log("console.log(new Date().getFullYear());--");
+console.log(new Date().getFullYear());
+console.log("----------------------------------");
+console.log("console.log(new Date().getTime());--");
+console.log(new Date().getTime());
+console.log("----------------------------------");
+console.log("console.log(new Date().getHours());--");
+console.log(new Date().getHours());
+console.log("----------------------------------");
+console.log("console.log(new Date().getMinutes());--");
+console.log(new Date().getMinutes());
+console.log("----------------------------------");
+console.log("console.log(new Date().getSeconds());--");
+console.log(new Date().getSeconds());
+console.log("----------------------------------");
+console.log("console.log(new Date().getMilliseconds());--");
+console.log(new Date().getMilliseconds());
+console.log("----------------------------------");
+console.log("console.log(new Date().getTimezoneOffset());--");
+console.log(new Date().getTimezoneOffset());
+console.log("----------------------------------");
+console.log("console.log(new Date().getUTCDate());--");
+console.log(new Date().getUTCDate());
+console.log("----------------------------------");
+console.log("console.log(new Date().getUTCMonth());--");
+console.log(new Date().getUTCMonth());
+console.log("----------------------------------");
+console.log("console.log(new Date().getUTCDay());--");
+console.log(new Date().getUTCDay());
+console.log("----------------------------------");
+console.log("console.log(new Date().getUTCFullYear());--");
+console.log(new Date().getUTCFullYear());
+console.log("----------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
